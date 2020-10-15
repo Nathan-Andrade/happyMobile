e1600a47a0532ba01fc46e18a0a1c2b201258773
Nathan Andrade <nathandulino2003@gmail.com>
2020-10-15 16:33:59 -0300

    app mobile

---
+++ README.md
@@ -0,0 +1,12 @@
+## Bibliotecas / Linguagens
+
+- React Native
+- Typescript
+- Expo
+- React Navigation
+- Expo-google-fonts
+- 
+
+## To run this app
+
+yarn start
---
+++ src/pages/OrphanagesMap.tsx
@@ -0,0 +1,109 @@
+import React from 'react';
+import { StyleSheet, View, Text, Dimensions, TouchableOpacity } from 'react-native';
+import MapView, { PROVIDER_GOOGLE, Marker, Callout } from 'react-native-maps'
+import { Feather } from '@expo/vector-icons';
+
+import mapMarker from '../images/map-marker.png';
+import { useNavigation } from '@react-navigation/native';
+
+export default function OrphanagesMap(){
+  const navigation = useNavigation();
+  
+  function handleNavigateToOrphanageDetails(){
+    navigation.navigate('OrphanageDetails')
+  }
+
+  return(
+    <View style={styles.container}>
+      <MapView provider={PROVIDER_GOOGLE} style={styles.map} initialRegion={{
+        latitude: -18.8571377,
+        longitude: -41.976786,
+        latitudeDelta: 0.008,
+        longitudeDelta: 0.008,
+      }}>
+        <Marker 
+          calloutAnchor={{
+            x: 2.7,
+            y: 0.8,
+          }}
+          icon={mapMarker}
+          coordinate={{
+            latitude: -18.8571377,
+            longitude: -41.976786,
+          }}
+        >
+          <Callout tooltip={true} onPress={handleNavigateToOrphanageDetails}>
+            <View style={styles.calloutContainer}>
+              <Text style={styles.calloutText}>Lar das Meninas</Text>
+            </View>
+          </Callout>
+        </Marker>
+      </MapView>
+
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>2 orfanatos encontrados</Text>
+        <TouchableOpacity style={styles.createOrphanageButton} onPress={() => {}}>
+          <Feather name='plus' size={20} color='#fff' />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height
+  },
+
+  calloutContainer: {
+    width: 160,
+    height: 46,
+    paddingHorizontal: 16,
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+    borderRadius: 16,
+    justifyContent: 'center',
+  },
+
+  calloutText: {
+    fontFamily: 'Nunito_700Bold',
+    color: '#0089a4',
+    fontSize: 14,
+  },
+
+  footer: {
+    position: 'absolute',
+    left: 24,
+    right: 24,
+    bottom: 32,
+
+    backgroundColor: '#fff',
+    borderRadius: 20,
+    height: 56,
+    paddingLeft: 26,
+
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+
+    elevation: 3,
+  },
+
+  footerText: {
+    fontFamily: 'Nunito_700Bold',
+    color: '#8fa7b3'
+  },
+
+  createOrphanageButton: {
+    width: 56,
+    height: 56,
+    backgroundColor: '#15c3d6',
+    borderRadius: 20,
+
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+});
